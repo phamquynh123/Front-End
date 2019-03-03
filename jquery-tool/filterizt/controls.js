e13735d90eb586204ae66f953bf6202ddc386a8d
phamquynh <phamquynh047@gmail.com>
2019-03-03 16:23:13 +0700

    test commit

--- jquery-tool/filterizt/controls.js
+++ jquery-tool/filterizt/controls.js
@@ -5,16 +5,16 @@ $(function() {
     $(this).addClass('active');
   });
   //Multifilter controls
-  $('.multifilter li').click(function() {
-    $(this).toggleClass('active');
-  });
-  //Shuffle control
-  $('.shuffle-btn').click(function() {
-    $('.sort-btn').removeClass('active');
-  });
-  //Sort controls
-  $('.sort-btn').click(function() {
-    $('.sort-btn').removeClass('active');
-    $(this).addClass('active');
-  });
+  // $('.multifilter li').click(function() {
+  //   $(this).toggleClass('active');
+  // });
+  // //Shuffle control
+  // $('.shuffle-btn').click(function() {
+  //   $('.sort-btn').removeClass('active');
+  // });
+  // //Sort controls
+  // $('.sort-btn').click(function() {
+  //   $('.sort-btn').removeClass('active');
+  //   $(this).addClass('active');
+  // });
 });
